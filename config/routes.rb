a7f8cef9a93360c2595025e9008175427ea7430b
John Doe <johndoe@example.com>
2017-04-03 10:02:53 +0000

    課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
     resources :blogs, only: [:index]
-    resources :poems, only: [:index]
+    resources :poems, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
